7612d8e115184f95e8fa25f9fadd355741f0595e
petrkucerak <45851215+petrkucerak@users.noreply.github.com>
2021-12-05 12:49:03 +0100

    Control ledbuildin

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,15 @@
 #include <Arduino.h>
 
 void setup() {
-  Serial.begin(115200);
+   Serial.begin(115200);
+   pinMode(LED_BUILTIN, OUTPUT);
 }
 
 void loop() {
-  Serial.printf("Hello world!\n");
-  delay(5000);
+   digitalWrite(LED_BUILTIN, HIGH);
+   Serial.printf("Light on!\n");
+   delay(5000);
+   Serial.printf("Light off!\n");
+   digitalWrite(LED_BUILTIN, LOW);
+   delay(5000);
 }
